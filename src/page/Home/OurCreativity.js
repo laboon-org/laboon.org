9e41904d0cd0a61a0a27f0fed51c31037b43be78
nguyen84254 <nguyen84254@st.vimaru.edu.vn>
2021-11-09 18:10:58 +0700

    Fix:Thanhtruot

--- src/page/Home/OurCreativity.js
+++ src/page/Home/OurCreativity.js
@@ -1,86 +1,126 @@
-import React from 'react'
-import productlist from '../../img/productlist.png'
-import productlist1 from '../../img/productlist1.png'
-import productlist2 from '../../img/productlist2.png'
-import productlist3 from '../../img/productlist3.png'
-import productlist4 from '../../img/productlist4.png'
-import productlist5 from '../../img/productlist5.png'
-import productlist6 from '../../img/productlist6.png'
-import productlist7 from '../../img/productlist7.png'
-import productlist8 from '../../img/x.png'
-import productlist9 from '../../img/x.png'
+import React from "react";
+import productlist from "../../img/productlist.png";
+import productlist1 from "../../img/productlist1.png";
+import productlist2 from "../../img/productlist2.png";
+import productlist3 from "../../img/productlist3.png";
+import productlist4 from "../../img/productlist4.png";
+import productlist5 from "../../img/productlist5.png";
+import productlist6 from "../../img/productlist6.png";
+import productlist7 from "../../img/productlist7.png";
+import productlist8 from "../../img/x.png";
+import productlist9 from "../../img/x.png";
 
-import productitem1 from "../../img/productitem1.png" 
-import productitem2 from "../../img/productitem2.png" 
-import productitem3 from "../../img/productitem3.png" 
-import productitem4 from "../../img/productitem4.png"
+import productitem1 from "../../img/productitem1.png";
+import productitem2 from "../../img/productitem2.png";
+import productitem3 from "../../img/productitem3.png";
+import productitem4 from "../../img/productitem4.png";
 
-import productitem5 from "../../img/productitem5.png" 
-import newproductlist from '../../img/newproductlist.png'
-import ItemProduct from './ItemProduct';
+import productitem5 from "../../img/productitem5.png";
+import newproductlist from "../../img/newproductlist.png";
+import ItemProduct from "./ItemProduct";
+import SpringOurcreativy from "./SpringOurcreativy.tsx";
+import { isMobile } from "react-device-detect";
 
 const OurCreativity = () => {
-   const linhvats = [
-     {
-        id: 1,
-        img: productitem1
-     },
-     {
-       id: 2,
-       img: productitem2
-     },
-     {
-       id: 3,
-       img: productitem3
-     },
-     {
-       id: 4,
-       img: productitem4
-     }
-   ]
-    return (
-        <div className="product__elu-full grid">
-              <div className='product__elu-background'>
-                <img src={productlist} alt="" className="productlist-img productlist-img1" />
-                <img src={productlist1} alt="" className="productlist-img productlist1" />
-                <img src={productlist2} alt="" className="productlist-img productlist2" />
-                <img src={productlist3} alt="" className="productlist-img productlist3" />
-                <img src={productlist4} alt="" className="productlist-img productlist4" />
-                <img src={productlist5} alt="" className="productlist-img productlist5" />
-                <img src={productlist6} alt="" className="productlist-img productlist6" />
-                <img src={productlist7} alt="" className="productlist-img productlist7" />
-                <img src={productlist8} alt="" className="productlist-img productlistx" />
-                <img src={productlist9} alt="" className="productlist-img productlistxleft" />
-              </div>
-              <div className="product__elu-content">
-                <div className="productlist__title col c-12">
-                  <h1>Our Creativity</h1>
-                  {/* <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</p> */}
-                </div>
-                <div className="angry">
-                  <div className="row">
-                    <div className="angry__left col l-4 c-12">
-                      <img src={newproductlist} alt="" className="angry__left-img1" />
-                      {/* <img src={productitem5} alt=""  className="productitem5"/> */}
-                    </div>
-                    <div className="angry__right col l-8 c-12">
-                      <div className="wrap__angry">
-                        <div className="wrap__angry-wrap-item row">
-                          <ItemProduct title={"Drop"} img={productitem1}></ItemProduct>
-                          <ItemProduct title={"Nest"} img={productitem2}></ItemProduct>
-                          <ItemProduct title={"Coming Soon"} img={productitem3}></ItemProduct>
-                          <ItemProduct title={"Coming Soon"} img={productitem4}></ItemProduct>
-                        </div>
-                        <div className="wrap__angry-btn">
-                          <img src={productitem5} alt="" />
-                        </div>
-                      </div>
-                    </div>
+  return (
+    <div className="product__elu-full grid">
+      <div className="product__elu-background">
+        <img
+          src={productlist}
+          alt=""
+          className="productlist-img productlist-img1"
+        />
+        <img
+          src={productlist1}
+          alt=""
+          className="productlist-img productlist1"
+        />
+        <img
+          src={productlist2}
+          alt=""
+          className="productlist-img productlist2"
+        />
+        <img
+          src={productlist3}
+          alt=""
+          className="productlist-img productlist3"
+        />
+        <img
+          src={productlist4}
+          alt=""
+          className="productlist-img productlist4"
+        />
+        <img
+          src={productlist5}
+          alt=""
+          className="productlist-img productlist5"
+        />
+        <img
+          src={productlist6}
+          alt=""
+          className="productlist-img productlist6"
+        />
+        <img
+          src={productlist7}
+          alt=""
+          className="productlist-img productlist7"
+        />
+        <img
+          src={productlist8}
+          alt=""
+          className="productlist-img productlistx"
+        />
+        <img
+          src={productlist9}
+          alt=""
+          className="productlist-img productlistxleft"
+        />
+      </div>
+      <div className="product__elu-content">
+        <div className="productlist__title col c-12">
+          <h1>Our Creativity</h1>
+          {/* <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</p> */}
+        </div>
+        <div className="angry">
+          <div className="row">
+            <div className="angry__left col l-4 c-12">
+              <img src={newproductlist} alt="" className="angry__left-img1" />
+              {/* <img src={productitem5} alt=""  className="productitem5"/> */}
+            </div>
+            <div className="angry__right col l-8 c-12">
+              <div className="wrap__angry">
+                { isMobile ? (
+                  <SpringOurcreativy></SpringOurcreativy>
+                ) : (
+                  <div className="wrap__angry-wrap-item row">
+                    <ItemProduct
+                      title={"Drop"}
+                      img={productitem1}
+                    ></ItemProduct>
+                    <ItemProduct
+                      title={"Nest"}
+                      img={productitem2}
+                    ></ItemProduct>
+                    <ItemProduct
+                      title={"Coming Soon"}
+                      img={productitem3}
+                    ></ItemProduct>
+                    <ItemProduct
+                      title={"Coming Soon"}
+                      img={productitem4}
+                    ></ItemProduct>
                   </div>
+                )}
+                <div className="wrap__angry-btn">
+                  <img src={productitem5} alt="" />
                 </div>
               </div>
+            </div>
           </div>
-    )
-}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default OurCreativity;
---
+++ src/page/Home/SpringOurcreativy.tsx
@@ -0,0 +1,118 @@
+import React, { useRef, useState } from 'react'
+import styled from 'styled-components';
+import { useSprings, animated } from '@react-spring/web'
+import useMeasure from 'react-use-measure'
+import { useDrag } from 'react-use-gesture'
+import clamp from 'lodash.clamp'
+import * as styless from './../../styles.module.css'
+
+import productitem1 from "../../img/productitem1.png";
+import productitem2 from "../../img/productitem2.png";
+import productitem3 from "../../img/productitem3.png";
+import productitem4 from "../../img/productitem4.png";
+
+const pages = [
+  // <img src={productitem1} alt="logo" />,
+  // <img src={productitem2} alt="logo" />,
+  // <img src={productitem3} alt="logo" />,
+  // <img src={productitem4} alt="logo" />,
+  'https://demo.laboon.org/static/media/productitem1.2798df72.png',
+  'https://demo.laboon.org/static/media/productitem2.77733997.png',
+  'https://demo.laboon.org/static/media/productitem3.fee8b991.png',
+  'https://demo.laboon.org//static/media/productitem4.8b8775dc.png'
+]
+
+function Viewpager(propso: any) {
+  const index = useRef(0)
+  const [ref, { width }] = useMeasure()
+  const realWidth = width * 1 / 3 + width;
+  const [props, api] = useSprings(
+    pages.length,
+    i => ({
+      x: i * realWidth,
+      scale: 1,
+      display: 'block',
+    }),
+    [realWidth]
+  )
+  const bind = useDrag(({ active, movement: [mx], direction: [xDir], distance, cancel }) => {
+    if (active && distance > realWidth / 2) {
+      index.current = clamp(index.current + (xDir > 0 ? -1 : 1), 0, pages.length - 1)
+      cancel()
+    }
+    api.start(i => {
+      propso.setIsSelected(index.current)
+      if (i < index.current - 1 || i > index.current + 1) return { display: 'none' }
+      const x = (i - index.current) * realWidth + (active ? mx : 0)
+      const scale = active ? 1 - distance / realWidth / 2 : 1
+      return { x, scale, display: 'block' }
+    })
+  })
+  return (
+    <div ref={ref} style={styles.wrapper}>
+      {props.map(({ x, display, scale }, i) => (
+        <animated.div className={styless.page} {...bind()} key={i} style={{ display, x, ...styles.page }}>
+          <animated.div style={{ scale, backgroundImage: `url(${pages[i]})` }} />
+        </animated.div>
+      ))}
+    </div>
+  )
+}
+const Mystyle = styled.div`
+  height: 112px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row; `
+const Dot = (props: any) => {
+  const { isSelect } = props
+  const color: string = isSelect ? '#E4B460' : '#fff'
+  return (
+    <span style={{ ...styles.dotSpan, backgroundColor: color }}></span>
+  )
+}
+
+
+export default function App() {
+  const [isSelected, setIsSelected] = useState(0);
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', }}>
+      <div style={styles.container}>
+        <Viewpager setIsSelected={setIsSelected} />
+      </div>
+      <Mystyle style={{ height: '10vh' }}>
+        {[0, 1, 2, 3].map((value) => {
+          return (<Dot isSelect={isSelected == value} />)
+        })}
+      </Mystyle>
+    </div>
+  )
+}
+const styles: { [key: string]: { [key: string]: string } } = {
+  wrapper: {
+    width: '50%',
+    height: '100%',
+  },
+  container: {
+    display: 'flex',
+
+    alignItems: 'center',
+    height: '30vh',
+    justifyContent: 'center',
+    position: 'relative'
+  },
+  page: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    willChange: 'transform',
+  },
+  dotSpan: {
+    height: '15px',
+    width: '15px',
+    borderRadius: '50%',
+    display: 'inline-block',
+    marginLeft: '10px',
+    marginRight: '10px',
+  }
+}
