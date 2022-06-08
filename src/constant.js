35bee396326954fd31e3a74cb57a26933ac58daa
DESKTOP-NNQ2MR6\HuyLA <huylahp97@gmail.com>
2022-06-08 09:30:12 +0700

    fix api link

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
-// export default {
-//   API:{
-//     url:'https://api-ap-northeast-1.graphcms.com/v2/ckx41ssik336s01w89hsk0rf5/master'
-//   }  
-// }
+export default {
+  API:{
+    url:'https://api-ap-northeast-1.graphcms.com/v2/ckx41ssik336s01w89hsk0rf5/master'
+  }  
+}
